f13cd567be0fdf9a90a56171bbcb37ed779e5bb5
Debraj Sengupta <debraj@lockstep.io>
2022-02-02 21:49:56 +0530

    xyz commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
+    libraryTarget: "umd",
+    library: "react_component",
   },
   module: {
     rules: [
