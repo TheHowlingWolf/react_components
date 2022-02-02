63fa6e87318ec2bc4ffa30edb27265d01ee1d7a5
Debraj Sengupta <debraj@lockstep.io>
2022-02-02 22:02:32 +0530

    xyz commit

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,20 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
   },
-  externals: {
-    react: "react",
-  },
+  externals: [
+    {
+      react: {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react",
+      },
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom",
+      },
+    },
+  ],
 };
