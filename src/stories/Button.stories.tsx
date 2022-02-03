56c527d71f39c558fa65a202b7674985c5180a6a
Debraj Sengupta <debraj@lockstep.io>
2022-02-03 16:17:20 +0530

    abc commit

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "../index";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
 
 export default {
   title: "Atomic/Button",
   component: Button,
+  decorators: [withDesign],
   argTypes: {
     variant: {
       options: ["contained", "outlined", "text"],
@@ -22,4 +24,11 @@ export const Primary = Template.bind({});
 Primary.args = {
   label: "This is button",
   variant: "outlined",
+};
+
+Primary.parameters = {
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/olNbhykb7uZ5cUu9extGmL/Untitled?node-id=1%3A2",
+  },
 };
